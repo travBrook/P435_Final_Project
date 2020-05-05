b0bd74db95718e859010a2a6e32aa48e48990a5b
unknown <dbvolz@iu.edu>
2020-05-04 21:24:26 -0400

    Logs and report statistics for each consistency model

--- driver.py
+++ driver.py
@@ -12,15 +12,17 @@ def run():
     print("\nMoving to clientel\n")
 
     #Spawn Client
-    proc_id = subprocess.Popen([sys.executable, './causalClient.py', config.CLIENT1_IP, config.MASTER_IP,'Client1', 'causal1'])
+    #proc_id = subprocess.Popen([sys.executable, './causalClient.py', config.CLIENT1_IP, config.MASTER_IP,'CausalClient1', 'causal1'])
+    #proc_id = subprocess.Popen([sys.executable, './causalClient.py', config.CLIENT6_IP, config.MASTER_IP,'CausalClient2',  'causal2'])
     #time.sleep(0.13)
-    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT2_IP, config.MASTER_IP,'Client2',  'linear1'])
-    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT3_IP, config.MASTER_IP,'Client3',  'seq2'])
-    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT4_IP, config.MASTER_IP,'Client4',  'linear2'])
-    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT5_IP, config.MASTER_IP,'Client5',  'event1'])
-    proc_id = subprocess.Popen([sys.executable, './causalClient.py', config.CLIENT6_IP, config.MASTER_IP,'Client6',  'causal2'])
-    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT7_IP, config.MASTER_IP,'Client7',  'event2'])
-    #proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT8_IP, config.MASTER_IP,'Client8',  'causal2'])
+    #proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT2_IP, config.MASTER_IP,'LinearClient1',  'linear1'])
+    #proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT4_IP, config.MASTER_IP,'LinearClient2',  'linear2'])
+    #proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT8_IP, config.MASTER_IP,'SeqClient1',  'seq1'])    
+   # proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT3_IP, config.MASTER_IP,'SeqClient2',  'seq2'])
+
+    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT5_IP, config.MASTER_IP,'EventualClient1',  'event1'])
+    proc_id = subprocess.Popen([sys.executable, './client.py', config.CLIENT7_IP, config.MASTER_IP,'EventualClient2',  'event2'])
+
     
    # time.sleep(0.13)
     '''
